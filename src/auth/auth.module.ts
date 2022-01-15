46a3a639dc06947703df5dca7cf527d74e7092e2
muktaking <yeakubinfire@gmail.com>
2022-01-15 22:37:50 +0600

    facebook strategy deleted

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,7 +7,6 @@ import { UserRepository } from 'src/users/user.repository';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { FacebookStrategy } from './facebook.strategy';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
@@ -26,7 +25,7 @@ const jwtConfig = config.get('jwt');
     UsersModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, LocalStrategy, FacebookStrategy],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
